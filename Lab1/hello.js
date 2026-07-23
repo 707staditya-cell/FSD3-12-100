4d179ea2cc1a2ef92ddf01cb84ce95a7f6603ba6
Aditya Singh <707staditya@gmail.com>
2026-07-23 11:53:52 +0530

    initial commit

---
+++ Lab1/hello.js
@@ -0,0 +1,14 @@
+console.log("Aditya Singh");
+
+// write a function to take two numbers and return
+// their sum
+function sum(a,b){
+    return a+b;
+}
+const add = (a,b) =>{
+    return a+b;
+const add2 = (a,b) => a+b ;
+
+console.log(sum(20,10));
+console.log(add(20,30));
+console.log(add2(40,50))
