5885dac77b61b3c087341a436acd4899cdcdc45f
Aditya Singh <707staditya@gmail.com>
2026-07-24 15:18:46 +0530

    "commit"

---
+++ Lab1/fun.js
@@ -0,0 +1,36 @@
+//Write a program to take any digit (0-9) and
+//return its in word
+//0-One
+//5-Five
+
+function word(a)
+if(a==0){
+    console.log("Zero");
+}
+if(a==1){
+    console.log("One");
+}
+if(a==2){
+    console.log("Two");
+}
+if(a==3){
+    console.log("Three");
+}
+if(a==4){
+    console.log("Four");
+}
+if(a==5){
+    console.log("Five");
+}
+if(a==6){
+    console.log("Six");
+}
+if(a==7){
+    console.log("Seven");
+}
+if(a==8){
+    console.log("Eight");
+}
+if(a==9){
+    console.log("Nine");
+}
--- Lab1/hello.js
+++ Lab1/hello.js
@@ -2,13 +2,21 @@ console.log("Aditya Singh");
 
 // write a function to take two numbers and return
 // their sum
-function sum(a,b){
-    return a+b;
+function sum(a, b) {
+  return a + b;
 }
-const add = (a,b) =>{
-    return a+b;
-const add2 = (a,b) => a+b ;
+const add = (a, b) => {
+  return a + b;
+};
+const add2 = (a, b) => {
+  return a + b;
+};
 
-console.log(sum(20,10));
-console.log(add(20,30));
-console.log(add2(40,50))
+console.log(sum(20, 10));
+console.log(add(20, 30));
+console.log(add2(40, 50));
+
+// frontend - flask , vue.5 , angular
+//common in the above four - html , css(bootstrap,tailwind) , java
+
+//backend - nodejs , springboot , fastapi , django , php
