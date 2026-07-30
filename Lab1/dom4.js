f20eff7bc6f25f6f9e64e57398bd47e1fd180b02
Aditya Singh <707staditya@gmail.com>
2026-07-30 11:22:08 +0530

    dom2.js

---
+++ Lab1/dom4.js
@@ -0,0 +1,12 @@
+//DOM -> Document Object Model
+import { EventEmitter } from "events";
+
+const form = new EventEmitter();
+
+form.on("submit", (uname,password) =>{
+    console.log("form submitted");
+    console.log(`user name: ${uname}`);
+    console.log(`user password: ${password}`);
+});
+
+form.emit("submit", "adityasingh.com", "1234");
