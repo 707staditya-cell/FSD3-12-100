64dcc6daadf2cd55c8bd17807190bf10314eb536
Aditya Singh <707staditya@gmail.com>
2026-09-18 14:49:41 +0530

    fbe fv

--- Lab3/prg5.js
+++ Lab3/prg5.js
@@ -18,6 +18,7 @@ const server = http.createServer((req, res) => {
     res.end("<h1>Contact Us</h1>");
   } else {
     res.statusCode = 404;
+    
     res.end(`
             <h1> Page not found</h1>
             <a href="/">Home</a>
